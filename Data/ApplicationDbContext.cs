344a9c60ed56d6f1cd46db25e2c8d277a135abc1
Elpacho27 <patrik27ocelic03@gmail.com>
2024-02-26 19:47:47 +0100

    Added entity framework Added category entity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BooksWebShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWebShop.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Category> Categories { get; set; }
+    
+
+
+}
