2f3c8ff6f789b98088abbc0a47f4e1eeb5014951
Elpacho27 <patrik27ocelic03@gmail.com>
2024-03-04 19:44:23 +0100

    Controllers and css

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BooksWebShop.Data;
+using BooksWebShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWebShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categorylist = _context.Categories.ToList();
+            return View(categorylist);
+        }
+    }
+}
