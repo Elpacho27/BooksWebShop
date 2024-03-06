124587d4d6d5a42120fbc12818fa0a479b84bacf
Elpacho27 <patrik27ocelic03@gmail.com>
2024-03-06 19:53:20 +0100

    Form/Form Validation Create Edit

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -18,5 +18,55 @@ namespace BooksWebShop.Controllers
             List<Category> categorylist = _context.Categories.ToList();
             return View(categorylist);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name.Length > 15)
+            {
+                ModelState.AddModelError("Name", "The name must not be longer than 15 characters");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? categoryId)
+        {
+            Category? category = _context.Categories.FirstOrDefault(c => c.Id == categoryId);
+            //Category? category1 = _context.Categories.Find(categoryId);
+            //Category? category2 = _context.Categories.Where(c=>c.Id== categoryId).FirstOrDefault();
+
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name.Length > 15)
+            {
+                ModelState.AddModelError("Name", "The name must not be longer than 15 characters");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BooksWebShop.Models;
 
@@ -6,7 +7,11 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
+    [MaxLength(30)]
     public string Name { get; set; }
+    [DisplayName("Display Name")]
+    [Range(1,30)]
+
     public int DisplayOrder { get; set; }
 
 
